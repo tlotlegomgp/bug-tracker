fda5636ca212b1f52f6e88bf07b856d94cd1dab0
Tlotlego Mogapi <58775165+Dark-Matter-crypto@users.noreply.github.com>
2020-12-19 20:35:13 +0200

    Added custom password reset and change templates

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -7,4 +8,11 @@ urlpatterns = [
     path('register/', views.register_view, name = "registration_page"),
     path('login/', views.login_view, name = "login_page"),
     path('logout/', views.logout_view, name = "logout"),
+
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name = 'account/password_change.html'), name = 'password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name = 'account/password_change_done.html'), name = 'password_change_done'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name = 'account/password_reset.html', email_template_name='account/password_reset_email.html', subject_template_name='account/password_reset_subject.txt'), name = 'password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name = 'account/password_reset_done.html'), name = 'password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name = 'account/password_reset_confirm.html'), name = 'password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name = 'account/password_reset_complete.html'), name = 'password_reset_complete'),
 ]
--- bugtracker/settings.py
+++ bugtracker/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 # Application definition
 
