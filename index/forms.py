42ed2794f513554d5c6fbb2e26d9d38c472af618
Tlotlego Mogapi <58775165+Dark-Matter-crypto@users.noreply.github.com>
2021-01-06 21:37:47 +0200

    Completed the message conversation implementation

--- index/forms.py
+++ index/forms.py
@@ -2,8 +2,8 @@ from django import forms
 
 
 class TodoForm(forms.Form):
-    todo = forms.CharField(max_length=80, widget=forms.TextInput(attrs={'class': 'form-control', 'name': "todo"}))
+    todo = forms.CharField(max_length=80, widget=forms.TextInput(attrs={'class': 'form-control', 'name': "todo"}), required=True)
 
 
 class ConversationForm(forms.Form):
-    message = forms.CharField(max_length=500, widget=forms.Textarea(attrs={'class': 'form-control', 'name': "message", 'style': 'max-height: 120px; min-height: 120px;'}))
+    message = forms.CharField(max_length=500, widget=forms.Textarea(attrs={'class': 'form-control', 'name': "message", 'style': 'max-height: 120px; min-height: 120px;'}), required=True)
