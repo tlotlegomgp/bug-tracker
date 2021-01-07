d9263ea338f9d626368060e9ebc60d1fcc2bf81d
Tlotlego Mogapi <58775165+Dark-Matter-crypto@users.noreply.github.com>
2021-01-07 09:19:44 +0200

    Changed the nav_bar messages filtering

--- index/custom_processors.py
+++ index/custom_processors.py
@@ -1,6 +1,8 @@
+from operator import attrgetter
 from django.shortcuts import get_object_or_404
+from django.db.models import Q
 from account.models import Profile
-from .models import DirectMessage, Alert
+from .models import DirectMessage, Alert, Conversation
 from .forms import ConversationForm
 
 
@@ -9,10 +11,25 @@ def profile_processor(request):
         user = request.user
         user_profile = get_object_or_404(Profile, user=user)
         unread_messages_count = DirectMessage.objects.filter(receiver=user_profile).filter(status='UNREAD').count()
-        user_messages = DirectMessage.objects.filter(receiver=user_profile).filter(status='UNREAD').order_by('-created_on')[:5]
-        user_alerts = Alert.objects.filter(user=user_profile).filter(status='UNREAD').order_by('-created_on')[:4]
         all_alerts = Alert.objects.filter(user=user_profile).filter(status='UNREAD').order_by('-created_on')[:30]
+        user_alerts = all_alerts[:4]
+        conversations = Conversation.objects.filter(Q(user_1=user_profile) | Q(user_2=user_profile))
+        unsorted_messages = []
+        for conv in conversations:
+            latest_message = DirectMessage.objects.filter(conversation = conv).order_by('-created_on').first()
+            unsorted_messages.append(latest_message)
+
+        user_messages = sorted(list(set(unsorted_messages)), key=attrgetter('created_on'), reverse=True)
+        nav_messages = user_messages[:5]
         form = ConversationForm()
-        return {'profile': user_profile, 'direct_messages': user_messages, 'alerts': user_alerts, 'all_alerts': all_alerts, 'unread_count': unread_messages_count, 'conversation_form': form}
+        return {
+            'profile': user_profile, 
+            'nav_messages': nav_messages,
+            'direct_messages': user_messages, 
+            'alerts': user_alerts, 
+            'all_alerts': all_alerts, 
+            'unread_count': unread_messages_count, 
+            'conversation_form': form
+            }
     else:
         return{}
